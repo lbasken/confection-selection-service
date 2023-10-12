602fadf91f027bde88a032a876021f8183e57584
Lorelei <lorelei@basken.com>
2023-10-12 19:56:10 -0400

    listening for 'contests' endpoint

--- functions/index.js
+++ functions/index.js
@@ -30,4 +30,15 @@ app.get("/newsfeed/:id", async (request, response) => {
     response.send(snapshot.data());
 });
 
+app.get("/contests", async (request, response) => {
+    const snapshot = await firebase.firestore().collection("contests").get();
+    const contests = snapshot.docs.map(document => document.data());
+    response.send(contests);
+});
+
+app.get("/contests/:id", async (request, response) => {
+    const snapshot = await firebase.firestore().collection("contests").doc(request.params.id).get();
+    response.send(snapshot.data());
+});
+
 exports.api = onRequest(app);
