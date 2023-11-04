56db7bef041adb2ebad2cc6f01f6c61c42b45daf
Dana Basken <dana@pridelands.com>
2023-11-04 11:35:16 -0400

    added EventsController

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const UserController = require("./src/controllers/UserController");
 const ContestController = require("./src/controllers/ContestController");
 const UserMiddleware = require("./src/middleware/UserMiddleware");
+const EventsController = require("./src/controllers/EventsController");
 
 const app = express();
 app.use(cors());
@@ -11,27 +12,6 @@ app.use(UserMiddleware.filter);
 
 UserController.start(app);
 ContestController.start(app);
-
-// app.get("/contestant", async (request, response) => {
-//     const snapshot = await firebase.firestore().collection("contestants").get();
-//     const contestants = snapshot.docs.map(document => document.data());
-//     response.send(contestants);
-// });
-//
-// app.get("/contestant/:id", async (request, response) => {
-//     const snapshot = await firebase.firestore().collection("contestants").doc(request.params.id).get();
-//     response.send(snapshot.data());
-// });
-//
-// app.get("/newsfeed", async (request, response) => {
-//     const snapshot = await firebase.firestore().collection("newsfeed").get();
-//     const newsfeed = snapshot.docs.map(document => document.data());
-//     response.send(newsfeed);
-// });
-//
-// app.get("/newsfeed/:id", async (request, response) => {
-//     const snapshot = await firebase.firestore().collection("newsfeed").doc(request.params.id).get();
-//     response.send(snapshot.data());
-// });
+EventsController.start(app);
 
 exports.api = onRequest(app);
---
+++ functions/src/controllers/EventsController.js
@@ -0,0 +1,44 @@
+const Firebase = require("../Firebase");
+
+class EventsController {
+
+  static start(app) {
+
+    app.get("/api/v1/events", (request, response) => {
+      response.setHeader("Content-Type", "text/event-stream");
+      response.setHeader("Cache-Control", "no-cache");
+      response.setHeader("Connection", "keep-alive");
+      response.setHeader("X-Accel-Buffering", "no");
+
+      const unsubscribe = Firebase.firestore.collection("contests").onSnapshot(snapshot => {
+        const changes = snapshot.docChanges().map(change => ({id: change.doc.ref.id, type: change.type}));
+        for (const change of changes) {
+          EventsController.send(response, {type: "contest", change: change.type, id: change.id});
+        }
+      });
+
+      EventsController.ping(response);
+      const interval = setInterval(() => {EventsController.ping(response)}, 30000);
+
+      request.on("close", () => {
+        console.log("close", unsubscribe);
+        unsubscribe();
+        clearInterval(interval);
+        response.destroy();
+      });
+    });
+
+  }
+
+  static ping(response) {
+    EventsController.send(response, {type: "ping"});
+  }
+
+  static send(response, message) {
+    message.timestamp = message.timestamp ?? Date.now();
+    response.write(`data: ${JSON.stringify(message)}\n\n`);
+  }
+
+}
+
+module.exports = EventsController;
